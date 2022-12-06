a35c7c6b104829d85c56fc0245ff54219151414f
Welkin Min <116010168@link.cuhk.edu.cn>
2022-12-07 01:29:31 +0900

    Update
    1. Remove the unnecessary package.
    2. Fix the button linkage problems.
    3. UI layout problem still exists. Black keys are created but hidden for now.
    4. Latency would be a big issue.

--- RIMS-2022/Assets/MusicControl.cs
+++ RIMS-2022/Assets/MusicControl.cs
@@ -3,41 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 public class MusicControl : MonoBehaviour
-{
-    public AudioSource C_Note;
-    public AudioSource Cs_Note;
-    public AudioSource D_Note;
-    public AudioSource Ds_Note;
-    public AudioSource E_Note;
-    public AudioSource F_Note;
-    public AudioSource Fs_Note;
-    public AudioSource G_Note;
-    public AudioSource Gs_Note;
-    public AudioSource A_Note;
-    public AudioSource B_Note;
-    public AudioSource Bs_Note;
-    public AudioSource C1_Note;
+{   
+    public AudioSource Source;
+    public AudioClip C_Note;
+    public AudioClip Cs_Note;
+    public AudioClip D_Note;
+    public AudioClip Ds_Note;
+    public AudioClip E_Note;
+    public AudioClip F_Note;
+    public AudioClip Fs_Note;
+    public AudioClip G_Note;
+    public AudioClip Gs_Note;
+    public AudioClip A_Note;
+    public AudioClip B_Note;
+    public AudioClip Bb_Note;
+    public AudioClip C1_Note;
 
     private void OnEnable() {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        // VisualElement white_keys = root.Query<VisualElement>("white_keys");
-        // VisualElement black_keys_1 = root.Query<VisualElement>("black_keys_1");
-        // VisualElement black_keys_2 = root.Query<VisualElement>("black_keys_2");
+        
 
+        root.Q<Button>("C").clicked += () => {
+            Source.PlayOneShot(C_Note);
+        };
 
-        Button C_Button = root.Query<Button>("C");
-        Debug.Log(C_Button);
-        // Button buttonCenter = root.Q<Button>("Center");
-        // Button buttonFrontLeft = root.Q<Button>("FrontLeft");
-        // Button buttonLeft = root.Q<Button>("Left");
-        // Button buttonFrontRight = root.Q<Button>("FrontRight");
-        // Button buttonRight = root.Q<Button>("Right");
+        root.Q<Button>("Cs").clicked += () => {
+            Source.PlayOneShot(Cs_Note);
+        };
 
-        C_Button.clicked += () => Cpressed();
+        root.Q<Button>("D").clicked += () => {
+            Source.PlayOneShot(D_Note);
+        };
+        
+        root.Q<Button>("Ds").clicked += () => {
+            Source.PlayOneShot(Ds_Note);
+        };
 
-        void Cpressed(){
-            Debug.Log("Preess");
-        }
+        root.Q<Button>("E").clicked += () => {
+            Source.PlayOneShot(E_Note);
+        };
+        
+        root.Q<Button>("F").clicked += () => {
+            Source.PlayOneShot(F_Note);
+        };
+        
+        root.Q<Button>("Fs").clicked += () => {
+            Source.PlayOneShot(Fs_Note);
+        };
+
+        root.Q<Button>("G").clicked += () => {
+            Source.PlayOneShot(G_Note);
+        };
+        
+        root.Q<Button>("Gs").clicked += () => {
+            Source.PlayOneShot(Gs_Note);
+        };
+
+        root.Q<Button>("A").clicked += () => {
+            Source.PlayOneShot(A_Note);
+        };
+        
+        root.Q<Button>("Bb").clicked += () => {
+            Source.PlayOneShot(Bb_Note);
+        };
+        
+        root.Q<Button>("B").clicked += () => {
+            Source.PlayOneShot(B_Note);
+        };
+        
+        root.Q<Button>("C1").clicked += () => {
+            Source.PlayOneShot(C1_Note);
+        };
+        
 
     }
 }
