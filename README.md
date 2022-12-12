9d0777a4ac28f44f68ac434edfc967fdad0d643d
Welkin Min <116010168@link.cuhk.edu.cn>
2022-12-12 16:44:02 +0900

    Update Readme, Add testing function in MidiControl.cs

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # rwis_unity_rep
+
 Repository for development of a Unity-based Android app, as part of the Real-World Interactive System (RWIS) course dispensed by Prof. Yuta SUGIURA of Keio University (2022 Fall Semester)
+
+# About the unity_branch
+
+The unity_branch is supported by [API MPTK - Playing MIDI by script - Maestro - Midi Player Tool Kit](https://paxstellar.fr/api-mptk-v2/).
+
+The basic idea is 
+
+1. load midi files as a list<MPTKEvent>,
+
+2. Control with keys,
+   
+   1. Key Down:
+      
+      1. Detect the number of keys,
+      
+      2. Form a onlist<MPTKEvent>,
+      
+      3. Play the onlist<MPTKEvent> and keep it on with duration = -1,
+      
+      4. Update the index of the list<MPTKEvent>,
+   
+   2. Key Up:
+      
+      1. Form a offlist<MPTKEvent>,
+      
+      2. Play the offlist<MPTKEvent> to cut off the note
+
+
+
+2022.12.12:
+
+Need to find way to precisely control Unity Input.
--- unity_branch/Assets/Scripts/MidiControl.cs
+++ unity_branch/Assets/Scripts/MidiControl.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MidiPlayerTK;
-
+using System.Linq;
 public class MidiControl : MonoBehaviour
 {
     public MidiFilePlayer midiFilePlayer;
     public MidiStreamPlayer midiStreamPlayer;
     private int midiEventListIndex;
     private List<MPTKEvent> midiEventList;
+    private HashSet<KeyCode> keysToCheck = new HashSet<KeyCode>((KeyCode[])System.Enum.GetValues(typeof(KeyCode)));
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +19,17 @@ public class MidiControl : MonoBehaviour
         midiEventList = midiLoad.MPTK_ReadMidiEvents();
         
         // Remove the event except for NoteOn
-        Debug.Log(midiEventList.Count);
+        Debug.Log("On Start: Number of MPTKEvent is " + midiEventList.Count);
         midiEventList.RemoveAll(item=> item.Command != MPTKCommand.NoteOn);
-        Debug.Log(midiEventList.Count);
-        Debug.Log(midiEventList[0]);
+        Debug.Log("On Start: Number of MPTKEvent after remove is " + midiEventList.Count);
+        // Debug.Log(midiEventList[0]);
 
-        // Unify the Velocity
+        // Unify the Velocity & Duration
         foreach(MPTKEvent midievent in midiEventList){
             midievent.Velocity = 95;
+            midievent.Duration = -1;
         }
+        
 
         // Debug.Log(nameof(MPTKCommand.NoteOn));
         midiEventListIndex = 0;
@@ -33,34 +37,40 @@ public class MidiControl : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        // Get Number of Key Pressed
+        // int numberOfKeysPressed;
+        // numberOfKeysPressed = keysToCheck.Count(key => Input.GetKey(key));
+        // Debug.Log(numberOfKeysPressed);
+        
+        // Muptiple Key Pressed Streaming Play Test
+        // if (Input.GetKeyDown(KeyCode.Space)){
+        //     midiStreamPlayer.MPTK_StartMidiStream();
+        //     List<MPTKEvent> subList = midiEventList.GetRange(1, 20);
+        //     midiStreamPlayer.MPTK_PlayEvent(subList);
+        // }
+
         if (Input.anyKeyDown)
         {   
-
-
-            // Multiple key down
-            // numberOfKeysPressed = 
-            // print(Input.inputString);
-
             // Testing on triggering a sequence of MPTKEvent
             midiStreamPlayer.MPTK_StartMidiStream();
             midiStreamPlayer.MPTK_PlayEvent(midiEventList[midiEventListIndex]);
-            Debug.Log(midiEventList[midiEventListIndex]);
-            midiEventListIndex += 1;
-            
-
-            // // Code triggering a single midi event.
-            // midiStreamPlayer.MPTK_StartMidiStream();
-            // MPTKEvent NotePlaying = new MPTKEvent() {
-            //     Command = MPTKCommand.NoteOn,
-            //     Value = 60, // play a C4 note
-            //     Channel = 0,
-            //     Duration = 1000, // one second
-            //     Velocity = 100 };
-            // midiStreamPlayer.MPTK_PlayEvent(NotePlaying);
-
-            // midiFilePlayer.MPTK_Play();
-            // Debug.Log(midiFilePlayer.MPTK_MidiEvents);
+            // Debug.Log(midiEventList[midiEventListIndex]);
+            midiEventListIndex += 1;  
         }
+        
+        
+        // // Code triggering a single midi event.
+        // midiStreamPlayer.MPTK_StartMidiStream();
+        // MPTKEvent NotePlaying = new MPTKEvent() {
+        //     Command = MPTKCommand.NoteOn,
+        //     Value = 60, // play a C4 note
+        //     Channel = 0,
+        //     Duration = 1000, // one second
+        //     Velocity = 100 };
+        // midiStreamPlayer.MPTK_PlayEvent(NotePlaying);
+
+        // midiFilePlayer.MPTK_Play();
+        // Debug.Log(midiFilePlayer.MPTK_MidiEvents);
     }
 }
